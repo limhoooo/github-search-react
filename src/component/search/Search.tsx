715832cc43676a0624d3a8398906445838e472a1
lim ho <dlagh123@gmail.com>
2023-04-13 10:39:21 +0900

    Revert "당근잡기 추가"
    This reverts commit 6f2171d0c9c240629e621c5fbbf36d7b53931539.

--- src/component/search/Search.tsx
+++ src/component/search/Search.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import searchIcon from '../../assets/search-icon.png';
-import { fetchRepositoryList, fetchRepository } from '../../store/reducers/search-slice';
+import { fetchRepositoryList } from '../../store/reducers/search-slice';
 import classes from './Search.module.css';
 import { useAppDispatch } from '../../hook/hooks';
 
@@ -14,16 +14,13 @@ const Search = () => {
   const onSearchValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
-  const onSearchSubmit = async (e: React.FormEvent) => {
+  const onSearchSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (!search.trim()) {
       setIsValid(true);
       return;
     }
-    const aaa = await dispatch(fetchRepositoryList(search.trim())).unwrap();
-    console.log(aaa);
-    await dispatch(fetchRepository({ owner: aaa, repo: 'aaa' }));
-
+    dispatch(fetchRepositoryList(search.trim()));
     setSearch('');
   };
   const validText = isValid ? '정확한 검색어를 입력해주세요' : 'Search Your Github';
